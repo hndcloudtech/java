ab2b85aca3c551578da6381ace869fea0f35d256
hndcloudtech <78495782+hndcloudtech@users.noreply.github.com>
2021-09-25 23:18:45 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,20 @@
-class Simple{
-public static void main(String args[]){
+@Before
+public void setup() {
+    Caches.BRANCH_METADATA.invalidateAll();
 
-System.out.printIn("Welcome to India");
+    jenkins = mock(Jenkins.class);
+
+    PowerMockito.mockStatic(Jenkins.class);
+    when(Jenkins.getInstance()).thenReturn(jenkins);
+
+    when(jenkins.getFullName()).thenReturn("");
+
+    job = mock(Job.class);
+    when(job.getParent()).thenReturn(jenkins);
+    when(job.getFullName()).thenReturn("BobsPipeline");
+    when(jenkins.getItemByFullName("BobsPipeline", Job.class)).thenReturn(job);
+
+    org = mock(BlueOrganization.class);
+    branch = new BranchImpl(org, job, new Link("foo"));
 }
-}
+ 
